544a63ae8723306bc7c44f0bb336859d08688471
templo1923 <temploluxury@gmail.com>
2025-09-20 09:48:00 -0500

    Add files via upload

---
+++ tele-latino-app/wp-content/uploads/uag-plugin/assets/3000/uag-js-3008.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-cb1447b4' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-f7b7d5a4' );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-6dafac51', {"layout":"circle","heading":"R\u00c1PIDO ","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":100,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":"","circleSize":190,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-79faf89a', {"layout":"circle","heading":"ESTABLE ","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":95,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":"","circleSize":190,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-31f4c5a6', {"layout":"circle","heading":"CONFIABLE ","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":100,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":"","circleSize":190,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-fecdfbe2', {"layout":"circle","heading":"RENTABLE ","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":98,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":"","circleSize":190,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-92e1047d', {"layout":"circle","heading":"SOPORTE","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":90,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":180,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-1a8330c5' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-faf49340' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-fb1e9f61' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-d49d6582' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-94b6bc9b' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-93c1b2d1' );
+});
+ });
