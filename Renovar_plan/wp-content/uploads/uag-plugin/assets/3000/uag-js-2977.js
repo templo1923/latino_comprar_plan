8dd472c9a2c0eb9bedae4ba39d0371ee0da19528
templo1923 <temploluxury@gmail.com>
2025-09-20 11:32:48 -0500

    Add files via upload

---
+++ Renovar_plan/wp-content/uploads/uag-plugin/assets/3000/uag-js-2977.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-6624c696' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-4ac318a8' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-4217710c' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-ab37fb88' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-e9b247fd' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-94b6bc9b' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-93c1b2d1' );
+});
+ });
